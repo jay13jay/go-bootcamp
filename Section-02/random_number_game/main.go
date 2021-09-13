fd12eb105734001112665a690d2946867d0d8199
Josh Hackney <josh.hackney13@gmail.com>
2021-09-13 09:43:36 -0400

    added unix timestamp which seeds random on program run

--- Section-02/random_number_game/main.go
+++ Section-02/random_number_game/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 	guess := 10
+	var count int
+	rand.Seed(time.Now().Unix())
 
 	for n := 0; n != guess; {
 		n = rand.Intn(guess + 1)
-		fmt.Printf("%d ", n)
+		if n == guess {
+			fmt.Printf("Guess: %d\nTarget: %d\nGuess is correct!\n", guess, n)
+			fmt.Printf("Answer took %d tries\n", count+1)
+		} else {
+			fmt.Printf("Guess: %d\nTarget: %d\nWrong answer... Try again\n", guess, n)
+			count++
+		}
 	}
-	fmt.Println()
 }
