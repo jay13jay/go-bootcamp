359e2b4cc4996871c6b7bc4b9c817bd6a1823355
Josh Hackney <josh.hackney13@gmail.com>
2021-09-13 10:53:30 -0400

    added game logic, need to switch guesses and target and take guess from user input

--- Section-02/random_number_game/main.go
+++ Section-02/random_number_game/main.go
@@ -7,20 +7,57 @@ import (
 )
 
 func main() {
-	target := 10
-	var count int
-	// u_time := time.Now().UnixNano()
+	// define our game parameters
+	var attempts int = 1 // Number of tries attempted
+	var tries int = 5    // amount of tries before Game Over
+	var target int = 10  // number the user has to guess
+	var guesses []int    // array of wrong answers tried
+
+	initGame(tries, target)
+
+	// Seed random using unix nanosecond timestamp
 	rand.Seed(time.Now().UnixNano())
 
-	fmt.Printf("Target: %d\nGuesses: ", target)
-	for n := 0; n != target; {
-		n = rand.Intn(target + 1)
-		if n == target {
-			fmt.Printf("\nGuess is correct!\n")
-			fmt.Printf("Answer took %d tries\n", count+1)
+	// Loop repeatedly until tries reaches 0
+	for guess := 0; attempts < tries; {
+		guess = rand.Intn(target + 1) // logic is currently backwords to simulate a user guessing
+
+		if checkNum(guess, target) {
+			guesses = append(guesses, guess) // add final guess to array
+
+			// Game Over - Victory
+			gameOver(true, guesses, attempts)
+			return
 		} else {
-			fmt.Printf("%d ", n)
-			count++
+			guesses = append(guesses, guess)
+			attempts++ // increment number of attempts counter
 		}
 	}
+	// Game Over - Defeat
+	gameOver(false, guesses, attempts)
+
+}
+
+func checkNum(guess int, target int) bool {
+	if guess == target {
+		return (true)
+	} else {
+		return (false)
+	}
+}
+
+func gameOver(win bool, guesses []int, attempts int) {
+	fmt.Println()
+	if win {
+		fmt.Println("You Win!")
+	} else {
+		fmt.Println("GAME OVER!!!")
+	}
+	fmt.Printf("Guesses: %d\n", guesses)
+	fmt.Printf("Number of Attempts: %d\n", attempts)
+}
+
+func initGame(tries int, target int) {
+	fmt.Printf("You have %d tries to guess the number!\n", tries)
+	fmt.Printf("Target: %d\n", target)
 }
